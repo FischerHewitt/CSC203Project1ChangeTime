c9702ce0f32a1d9856e91125d925813e0c7ad728
fischerhewitt <fihewitt@calpoly.edu>
2026-04-08 08:44:01 -0700

    fischer third commit

--- MoveEventTimeFischer/src/MainFischer.java
+++ MoveEventTimeFischer/src/MainFischer.java
@@ -13,14 +13,14 @@ import java.util.ArrayList;
 
 
 public static void main(String[] args) {
-    ArrayList<ArrayList<String>> calendar = new ArrayList<>();
-    calendar.add(0, new ArrayList<String>());
-    calendar.add(1, new ArrayList<String>());
-    calendar.add(2, new ArrayList<String>());
-    calendar.add(3, new ArrayList<String>());
-    calendar.add(4, new ArrayList<String>());
-    calendar.add(5, new ArrayList<String>());
-    calendar.add(6, new ArrayList<String>());
+    ArrayList< ArrayList<String> > calendar = new ArrayList<>();
+    calendar.add(0, new ArrayList<>());
+    calendar.add(1, new ArrayList<>());
+    calendar.add(2, new ArrayList<>());
+    calendar.add(3, new ArrayList<>());
+    calendar.add(4, new ArrayList<>());
+    calendar.add(5, new ArrayList<>());
+    calendar.add(6, new ArrayList<>());
     calendar.get(0).add("Math class at 10:00am");
     calendar.get(0).add("Gym at 2:00pm");
     calendar.get(2).add("Dance class at 9:00am");
@@ -31,21 +31,21 @@ public static void main(String[] args) {
     testCases(calendar);
 }
 
-public static void moveEventTime(ArrayList<ArrayList<String>> calendar,String newTime, String event ){
-    Object[] found_dayIdx_eventIdx = getEventTime(calendar, event);
-    boolean found = (boolean) found_dayIdx_eventIdx[0];
-    int day_idx = (int) found_dayIdx_eventIdx[1];
-    int event_idx = (int) found_dayIdx_eventIdx[2];
-    String event_name = (String) found_dayIdx_eventIdx[3];
-    String oldTime = (String) found_dayIdx_eventIdx[4];
-    String[] days_of_the_week = {"Monday", "Tuesday", "Wednesday", "Thursday",
+public static void moveEventTime(ArrayList< ArrayList<String> > calendar,String newTime, String event ){
+    Object[] found_dayIdx_eventIdx_eventName_oldTime = getEventTime(calendar, event);
+    boolean found = (boolean) found_dayIdx_eventIdx_eventName_oldTime [0];
+    int dayIdx = (int) found_dayIdx_eventIdx_eventName_oldTime [1];
+    int eventIdx = (int) found_dayIdx_eventIdx_eventName_oldTime [2];
+    String eventName = (String) found_dayIdx_eventIdx_eventName_oldTime [3];
+    String oldTime = (String) found_dayIdx_eventIdx_eventName_oldTime [4];
+    String[] daysOfTheWeek = {"Monday", "Tuesday", "Wednesday", "Thursday",
             "Friday", "Saturday", "Sunday"};
 
     if (!found){
         System.out.println("Event not found");
     } else {
-        boolean is_a_conflict = checkForConflict(calendar.get(day_idx), newTime);
-        if (is_a_conflict) {
+        boolean isAConflict = checkForConflict(calendar.get(dayIdx), newTime);
+        if (isAConflict) {
             boolean running = true;
             do {
                 System.out.println("There is a Conflict. Do you still want to add this event? " +
@@ -53,7 +53,7 @@ public static void moveEventTime(ArrayList<ArrayList<String>> calendar,String ne
                 Scanner myScanner = new Scanner(System.in);
                 String userInput = myScanner.next();
                 if (userInput.toLowerCase().equals("yes")){
-                    is_a_conflict = false;
+                    isAConflict = false;
                     running = false;
                 } else if ((userInput.toLowerCase().equals("no"))) {
                     running = false;
@@ -64,7 +64,7 @@ public static void moveEventTime(ArrayList<ArrayList<String>> calendar,String ne
             } while (running);
         }
 
-        if (!is_a_conflict) {
+        if (!isAConflict) {
             //deleteEvent(calendar, event_name);
             //addEvent(calendar, days_of_the_week[day_idx], event_name, newTime);
             System.out.println("Event Has Been Changed");
@@ -78,29 +78,29 @@ public static void moveEventTime(ArrayList<ArrayList<String>> calendar,String ne
 
 
 public static Object[] getEventTime(ArrayList<ArrayList<String>> calendar, String event){
-    int day_idx = 0;
-    while (day_idx < 7){
-        ArrayList<String> dayItems = calendar.get(day_idx);
-        int event_idx = 0;
-        while (event_idx < dayItems.size()){
+    int dayIdx = 0;
+    while (dayIdx < 7){
+        ArrayList<String> dayItems = calendar.get(dayIdx);
+        int eventIdx = 0;
+        while (eventIdx < dayItems.size()){
 
-            String[] event_array = dayItems.get(event_idx).split(" at ");
+            String[] eventArray = dayItems.get(eventIdx).split(" at ");
 
-            if (event_array[0].equals(event)) {
-                return new Object[]{true, day_idx, event_idx, event_array[0], event_array[1]};
+            if (eventArray[0].equals(event)) {
+                return new Object[]{true, dayIdx, eventIdx, eventArray[0], eventArray[1]};
             }
 
-            event_idx++;
+            eventIdx++;
         }
 
-        day_idx++;
+        dayIdx++;
     }
     return new Object[]{false, -1, -1, "N", "N"};
 }
 
-public static boolean checkForConflict(ArrayList<String> day_events, String newTime){
-    for (int idx = 0; idx < day_events.size(); idx++){
-        String[] event_array = day_events.get(idx).split(" at ");
+public static boolean checkForConflict(ArrayList<String> dayEvents, String newTime){
+    for (int idx = 0; idx < dayEvents.size(); idx++){
+        String[] event_array = dayEvents.get(idx).split(" at ");
         if (event_array[1].equals(newTime)){
             return true;
         }
@@ -134,6 +134,9 @@ public static void testCases(ArrayList<ArrayList<String>> calendar){
     System.out.println("Test: 06");
     moveEventTime(calendar, "9:00am", "Dance class");
 
-    System.out.println("\nTest: 07");
+    System.out.println("Test: 07");
+    moveEventTime(calendar, "9:00am", "Dance class");
+
+    System.out.println("\nTest: 08");
     moveEventTime(calendar, "9:00am", "Dance Class");
 }
